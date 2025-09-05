c4bd31407a4e7df647fa026b55e9f76a451a0b5e
Emden T Botin <Emden.T.Botin@gds.ey.com>
2025-09-05 08:01:08 +0800

    Update Case Scenario

--- Emden/Week1/case-scenario/main.py
+++ Emden/Week1/case-scenario/main.py
@@ -7,27 +7,26 @@ from file_reader import file_reader
 from file_writer import file_writer
 
 try:
-    # Initialize file_reader and extract Excel data
-    reader = file_reader()
-    excel_path = "C:\\Users\\JV833VJ\\OneDrive - EY\\Documents\\Python Lessons\\PythonTraining.xlsx"
-    sheet_name = "Employee"  # Change if needed
 
-    success = reader.read_excel(file_path=excel_path, sheet_name=sheet_name)
+    reader = file_reader()
+    success = reader.read_excel()
 
     if success:
         content = reader.get_content()
         
-        # Initialize file_writer and write to JSON
         writer = file_writer(content)
-        json_path = "C:\\Users\\JV833VJ\\OneDrive - EY\\Documents\\Python Lessons\\output.json"
-        write_success = writer.write_to_json(json_path)
+        write_success = writer.write_to_file("output.txt")
         
         if write_success:
-            print("JSON file written successfully.")
+            print("File written successfully.")
         else:
-            print("Failed to write the JSON file.")
+            print("Failed to write the file.")
     else:
-        print("Failed to read the Excel file.")
+        print("Failed to read the file.")
+
 
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"An error occurred: {e}")    
+
+
+
